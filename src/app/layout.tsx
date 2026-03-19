fe2e04e6b94b5f589ad486b269383a31dcbb42dc
marcos-acosta <macosta1625@gmail.com>
2026-03-19 00:08:02 -0400

    More nits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "wheredown.town",
   description: "Where downtown?",
+  themeColor: "#1f1f1f",
 };
 
 export default function RootLayout({
