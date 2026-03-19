ba599f5d0210dbab39e3dbe4e6d25235a4f15969
marcos-acosta <macosta1625@gmail.com>
2026-03-19 00:17:23 -0400

    Let's try that

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Literata } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const literata = Literata({
+  variable: "--font-literata",
   subsets: ["latin"],
 });
 
@@ -28,7 +23,7 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-title" content="downtown" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={literata.variable}>
         {children}
       </body>
     </html>
