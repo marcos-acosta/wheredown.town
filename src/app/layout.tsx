6fc196ca62650b3ff62c53355e1bac3123f9bd4c
marcos-acosta <macosta1625@gmail.com>
2026-03-19 12:37:49 -0400

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Literata } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const literata = Literata({
@@ -23,9 +24,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="downtown" />
         <meta name="theme-color" content="#1f1f1f" />
       </head>
-      <body className={literata.variable}>
-        {children}
-      </body>
+      <GoogleAnalytics gaId="G-4MRZXXQ6K7" />
+      <body className={literata.variable}>{children}</body>
     </html>
   );
 }
